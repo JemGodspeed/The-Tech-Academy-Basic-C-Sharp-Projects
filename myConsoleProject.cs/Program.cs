5dff4894717dd4e95c725414ceacf64ae87b54d3
JemGodspeed <cemkadir_92@hotmail.com>
2024-12-18 20:51:52 -0500

    Add files via upload

---
+++ myConsoleProject.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello "+name+"!");
+            Console.Read();
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+
+        }
+    }
+
+}
