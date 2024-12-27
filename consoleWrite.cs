c85551409adfed38cf390f10545e1e266a4a2469
JemGodspeed <cemkadir_92@hotmail.com>
2024-12-27 17:10:32 -0500

    Delete consoleWrite.cs

--- consoleWrite.cs
+++
@@ -1,5 +0,0 @@
-﻿Build started at 8:26 PM...
-1>------ Build started: Project: myConsoleProject.cs, Configuration: Debug Any CPU ------
-1>  myConsoleProject.cs -> C:\Users\cem\Basic_C#_Programs\myConsoleProject.cs\myConsoleProject.cs\bin\Debug\myConsoleProject.cs.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 8:26 PM and took 00.193 seconds ==========
