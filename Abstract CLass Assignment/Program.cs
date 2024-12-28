9196becea3a1125c58679561d925e03330b2e97d
JemGodspeed <cemkadir_92@hotmail.com>
2024-12-27 19:37:28 -0500

    Add files via upload
    added abstract class assignment

---
+++ Abstract CLass Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Abstract_CLass_Assignment
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public abstract void SayName();
+    }
+
+}
+
+
---
+++ Abstract CLass Assignment/program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Abstract_CLass_Assignment
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Employee Sam = new Employee() { firstName = "sample", lastName = "student" };
+            Sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
